a822a4234a72f966449fcdc688cd641ef3d2b0be
Rifat Khan <rifat6633@gmail.com>
2024-03-02 14:39:17 +0600

    create: sign up form

--- app/(auth)/auth/sign-up/page.tsx
+++ app/(auth)/auth/sign-up/page.tsx
@@ -1,3 +1,20 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import SignUpForm from "./_parts/sign-up-form";
+
 export default function Page() {
-  return <main>Page</main>;
+  return (
+    <main className="min-h-screen grid place-content-center">
+      <Card className="w-96">
+        <CardHeader>
+          <CardTitle className="text-lg">
+            Create an account with Lucia Auth
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <SignUpForm />
+        </CardContent>
+      </Card>
+    </main>
+  );
 }
