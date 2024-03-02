1a42cc3d4a26ef97ff8f50566ea91880acf5ba3e
Rifat Khan <rifat6633@gmail.com>
2024-03-02 14:47:08 +0600

    create: sign in form

--- app/(auth)/auth/sign-in/page.tsx
+++ app/(auth)/auth/sign-in/page.tsx
@@ -1,3 +1,39 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import SignInForm from "./_parts/sign-in-form";
+
 export default function Page() {
-  return <div>Page</div>;
+  return (
+    <main className="min-h-screen grid place-content-center">
+      <Card className="w-96">
+        <CardHeader>
+          <CardTitle className="text-lg">
+            Sign in to your account with Lucia Auth
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <SignInForm />
+        </CardContent>
+
+        <CardFooter>
+          <p>
+            Don&apos;t have an account?{" "}
+            <Link
+              href={"/auth/sign-up"}
+              className="font-semibold text-sm hover:underline"
+            >
+              Sign In
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </main>
+  );
 }
