ec721a5064b4d4f94f451081a135e0b10a638a74
Rifat Khan <rifat6633@gmail.com>
2024-03-03 12:20:22 +0600

    complete: user profile page and add secret page link

--- app/(user-profile)/[username]/page.tsx
+++ app/(user-profile)/[username]/page.tsx
@@ -1,3 +1,23 @@
-export default function Page({ params }: { params: { username: string } }) {
-  return <div>{params.username}</div>;
+import { Button } from "@/components/ui/button";
+import { validateRequest } from "@/lib/auth";
+import Link from "next/link";
+
+export default async function Page({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const { user } = await validateRequest();
+
+  return (
+    <main className="min-h-screen grid place-content-center">
+      <h1>{params.username}</h1>
+
+      {user?.username === params.username && (
+        <Button asChild>
+          <Link href={`/${user?.username}/secret`}>Secret Page</Link>
+        </Button>
+      )}
+    </main>
+  );
 }
