7f83936ead2962a0faf3888fc4ef351bac07d248
Rifat Khan <rifat6633@gmail.com>
2024-03-03 11:30:48 +0600

    add: signin state info and import logout button

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,31 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { validateRequest } from "@/lib/auth";
+import Logout from "@/components/logout";
 
-export default function Home() {
+export default async function Home() {
+  const { user } = await validateRequest();
+  console.log(user);
+
   return (
     <main className="min-h-screen grid place-content-center gap-2">
-      <Button asChild>
-        <Link href={"/auth/sign-up"}>Sign Up</Link>
-      </Button>
+      {user ? (
+        <>
+          <h1>User Name: {user.username}</h1>
 
-      <Button variant={"secondary"} asChild>
-        <Link href={"/auth/sign-in"}>Sign In</Link>
-      </Button>
+          <Logout />
+        </>
+      ) : (
+        <>
+          <Button asChild>
+            <Link href={"/auth/sign-up"}>Sign Up</Link>
+          </Button>
+
+          <Button variant={"secondary"} asChild>
+            <Link href={"/auth/sign-in"}>Sign In</Link>
+          </Button>
+        </>
+      )}
     </main>
   );
 }
