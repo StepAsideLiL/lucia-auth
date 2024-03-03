c4a46749d9b9e422bcd55fcd59a9d0257d0c2e18
Rifat Khan <rifat6633@gmail.com>
2024-03-03 13:58:01 +0600

    create: info page

---
+++ app/info/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <main className="min-h-screen grid place-content-center">
+      <h1 className="text-2xl">
+        This page is not user profile page. Even though it has same url path as
+        the user profile page
+      </h1>
+    </main>
+  );
+}
